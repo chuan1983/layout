b2fdc381e2175e215091ee788c839eecae078fa8
chuan1983 <chuan1983126@gmail.com>
2016-09-12 11:58:58 +0800

    9/12m5

---
+++ app/src/main/java/org/iii/tw/layouttest/WelcomeActivity.java
@@ -0,0 +1,29 @@
+package org.iii.tw.layouttest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private View img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        img = findViewById(R.id.weclome_img);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoMain();
+            }
+        });
+    }
+    private void gotoMain(){
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+        finish();
+    }
+}
